692df2205865b86fc1a4832e175573c2b249fdff
Firecatcher_303 <therockhound5833@outlook.com>
2026-01-02 11:47:10 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import re
+import os
+from discord.utils import escape_markdown, escape_mentions
 
 # ---------------------- CONFIG ----------------------
 intents = discord.Intents.default()
@@ -9,11 +11,18 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Words the moderation bot blocks
-BAD_WORDS = ["fuck", "shit", "bitch", "67", "skibidi", "ass", "nigger", "dick", "niger"]
+BAD_WORDS = [
+    "fuck", "shit", "bitch", "67", "skibidi",
+    "ass", "nigger", "dick", "niger"
+]
 
 # Channel IDs
-STARTUP_CHANNEL_ID = 1455793643670732972
-EXEMPT_CHANNELS = [1455632644967502000, 1455281535329505350]
+STARTUP_CHANNEL_ID = 1456677007940259921
+LOG_CHANNEL_ID = 1456677007940259921
+EXEMPT_CHANNELS = [
+    1455632644967502000,
+    1455281535329505350
+]
 
 # Regex to ignore custom emojis like <:name:id> or <a:name:id>
 CUSTOM_EMOJI_PATTERN = re.compile(r"<a?:\w+:\d+>")
@@ -25,7 +34,7 @@ async def on_ready():
 
     channel = bot.get_channel(STARTUP_CHANNEL_ID)
     if channel:
-        await channel.send('**Bot started. Nobody can cuss, say "67", or say "skibidi" in any of their sentences.**')
+        await channel.send("Restarted.")
 
 @bot.event
 async def on_message(message):
@@ -38,18 +47,41 @@ async def on_message(message):
         await bot.process_commands(message)
         return
 
-    # Allow messages with attachments (images/GIFs) without deleting
+    # Allow messages with attachments (images/GIFs)
     if message.attachments:
         await bot.process_commands(message)
         return
 
-    # Remove custom emojis from message before checking
-    text = CUSTOM_EMOJI_PATTERN.sub("", message.content.lower())
+    # Remove custom emojis before checking
+    cleaned_text = CUSTOM_EMOJI_PATTERN.sub("", message.content.lower())
 
-    # Split message into words and check for bad words
-    words = re.findall(r"\b\w+\b", text)
+    # Split into words
+    words = re.findall(r"\b\w+\b", cleaned_text)
+
     if any(word in BAD_WORDS for word in words):
-        await message.delete()
+        original_content = message.content
+        author = message.author
+        channel_name = message.channel.mention
+
+        try:
+            await message.delete()
+
+            log_channel = bot.get_channel(LOG_CHANNEL_ID)
+            if log_channel:
+                safe_content = escape_mentions(
+                    escape_markdown(original_content)
+                )
+
+                await log_channel.send(
+                    f"Message Deleted\n"
+                    f"**User:** {author}\n"
+                    f"**Channel:** {channel_name}\n"
+                    f"**Message:** \"{safe_content}\"",
+                    silent=True
+                )
+
+        except discord.Forbidden:
+            pass
 
     await bot.process_commands(message)
 
@@ -57,4 +89,3 @@ async def on_message(message):
 import os
 
 bot.run(os.environ["DISCORD_TOKEN"])
-
